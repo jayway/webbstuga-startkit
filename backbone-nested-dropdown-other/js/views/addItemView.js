12778f7f1808a7105f99b15db3d6054496b4337b
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2014-09-24 09:47:46 +0200

    Add exercise 'backbone-nested-dropdown-other'

---
+++ backbone-nested-dropdown-other/js/views/addItemView.js
@@ -0,0 +1,33 @@
+App.views.addItemView = (function(){
+
+  return Backbone.View.extend({
+
+    initialize: function(){
+      _.bindAll(this);
+      this.item = {};
+    },
+
+    load: function(){
+      // TODO: This should load whatever the render function needs and return a
+      // promise.
+
+      return App.api.getItemPriorities().done(function () {
+
+      });
+    },
+
+    render: function(){
+      var template = $("#add-item-template").text();
+      var rendered = _.template(template, {
+        // Here you add the properties that you want to expose to the template.
+        item: this.item
+      });
+      $(".template-container").html(rendered);
+    },
+
+    loadAndRender: function() {
+      this.load().done(this.render);
+    }
+  });
+}());
+
