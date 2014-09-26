184f70b40a319abfe370869287c4cc95817cf28c
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2014-09-26 08:06:14 +0200

    Remove test code in jasmine-in-memory to make it a bit harder

--- jasmine-in-memory-api/test/spec/test.js
+++ jasmine-in-memory-api/test/spec/test.js
@@ -25,39 +25,11 @@
         });
 
         describe('save', function () {
-            beforeEach(function(){
-                App.api.clear();
-            });
-
-            xit('saves the item', function () {
-               var brewBeer = App.api.save({ name: 'Brew Beer' });
-                var items = App.api.getAll();
-                expect(items).toContain(brewBeer);
-            });
+          // TODO
         });
 
         describe('remove', function () {
-            var brewBeer;
-
-            beforeEach(function(){
-                App.api.clear();
-                brewBeer = App.api.save({ name: 'Brew Beer' });
-            });
-
-            xit('removes the item if it exists', function (id) {
-                var success = App.api.remove(brewBeer.id);
-
-                expect(success).toBe(true);
-
-                var items = App.api.getAll();
-                expect(items).not.toContain(brewBeer);
-            });
-
-            xit('returns false if the items does not exist', function (id) {
-                var success = App.api.remove(-1);
-
-                expect(success).toBe(false);
-            });
+          // TODO
         });
     });
 })();
